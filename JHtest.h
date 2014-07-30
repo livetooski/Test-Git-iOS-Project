c3bb2dacc37c8969bddf5866375a7d325add4697
Jacob Hopkins <jacobhopkins@Jacobs-MacBook-Pro.local>
2014-07-30 13:13:20 -0600

    Added a test class

---
+++ JHtest.h
@@ -0,0 +1,13 @@
+//
+//  JHtest.h
+//  gitTest
+//
+//  Created by Jacob Hopkins on 7/30/14.
+//  Copyright (c) 2014 Jacob Hopkins. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface JHtest : NSObject
+
+@end
---
+++ JHtest.m
@@ -0,0 +1,13 @@
+//
+//  JHtest.m
+//  gitTest
+//
+//  Created by Jacob Hopkins on 7/30/14.
+//  Copyright (c) 2014 Jacob Hopkins. All rights reserved.
+//
+
+#import "JHtest.h"
+
+@implementation JHtest
+
+@end
